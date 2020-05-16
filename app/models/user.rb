631a7764a38a633c4ac4d80c1c1c64bf35779edc
UptightGiraffe <physician2008@gmail.com>
2020-05-16 11:08:46 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  validates :user_name,
+            presence: true,
+            length: { minimum: 3, maximum: 25 },
+            uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})\z/
+  validates :email,
+            presence: true,
+            length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 
 end
