1a711f0c1f13dd16ab86af2c46452ac8ae40a532
UptightGiraffe <physician2008@gmail.com>
2020-08-16 16:45:36 -0400

    add user log in and log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "Email and password combination not found"
+      render 'new'
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+      flash[:notice] = "Logged out"
+      redirect_to root_path
+    else
+      flash[:alert] = "Log in first before log out"
+      redirect_to root_path
+    end
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   resources :blogs
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 
 end
